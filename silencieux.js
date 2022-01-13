e6e79b9a41d6f3c6822b83edea6fb00e031903e4
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2022-01-13 22:37:40 +0100

    Create files via upload

---
+++ silencieux.js
@@ -0,0 +1,57 @@
+var e = ['echauffement/forward-leg-swings.jpg', 'echauffement/heels-to-buttocks.jpg', 'echauffement/high-knee-run.jpg', 'echauffement/lateral-leg-swings.jpg', 'echauffement/running.jpg', 'echauffement/walking-lunge.jpg'];
+var a = ['abdos/alternative-legs-arms-raises.jpg', 'abdos/criss-cross-crunch.jpg', 'abdos/crunch-oblique.jpg', 'abdos/crunch.jpg', 'abdos/double-legs-circles.jpg', 'abdos/flutter-kicks.jpg', 'abdos/high-plank.jpg', 'abdos/leg-pull-in-knee-up.jpg', 'abdos/lying-leg-raise.jpg', 'abdos/plank-downward-dog.jpg', 'abdos/plank-side-knee-tuck.jpg', 'abdos/plank-side.jpg', 'abdos/plank.jpg', 'abdos/russian-twists-obliques.jpg', 'abdos/side-plank-hip-abduction.jpg', 'abdos/superman.jpg', 'abdos/swimmer-swimming.jpg'];
+var b_c = ['bras_cardio/dips-bench.jpg', 'bras_cardio/heels-to-buttocks.jpg', 'bras_cardio/high-knee-run.jpg', 'bras_cardio/pushup.jpg', 'bras_cardio/running.jpg', 'bras_cardio/supine-reverse-plank.jpg', 'bras_cardio/wide-pushups.jpg'];
+var j = ['jambes/air-squat.jpg', 'jambes/calves-step-bar.jpg', 'jambes/chair-iso-squat.jpg', 'jambes/frog-squats.jpg', 'jambes/glutes-leg-kickback.jpg', 'jambes/lunge.jpg', 'jambes/walking-lunge.jpg'];
+
+var n = 0;
+const img = document.getElementById('img');
+var entrainement = 'silencieux\n\n';
+var l = ['a','j','b_c'];
+var pre = 'j';
+
+function nouveau_a(){
+    a = ['abdos/alternative-legs-arms-raises.jpg', 'abdos/criss-cross-crunch.jpg', 'abdos/crunch-oblique.jpg', 'abdos/crunch.jpg', 'abdos/double-legs-circles.jpg', 'abdos/flutter-kicks.jpg', 'abdos/high-plank.jpg', 'abdos/leg-pull-in-knee-up.jpg', 'abdos/lying-leg-raise.jpg', 'abdos/plank-downward-dog.jpg', 'abdos/plank-side-knee-tuck.jpg', 'abdos/plank-side.jpg', 'abdos/plank.jpg', 'abdos/russian-twists-obliques.jpg', 'abdos/side-plank-hip-abduction.jpg', 'abdos/superman.jpg', 'abdos/swimmer-swimming.jpg'];
+}
+
+function nouveau_b_c(){
+    b_c = ['bras_cardio/dips-bench.jpg', 'bras_cardio/heels-to-buttocks.jpg', 'bras_cardio/high-knee-run.jpg', 'bras_cardio/pushup.jpg', 'bras_cardio/running.jpg', 'bras_cardio/supine-reverse-plank.jpg', 'bras_cardio/wide-pushups.jpg'];
+}
+
+function nouveau_j(){
+    j = ['jambes/air-squat.jpg', 'jambes/calves-step-bar.jpg', 'jambes/chair-iso-squat.jpg', 'jambes/frog-squats.jpg', 'jambes/glutes-leg-kickback.jpg', 'jambes/lunge.jpg', 'jambes/walking-lunge.jpg'];
+}
+    
+
+function nouveau(){
+    n+=1;
+    if (n<=4){
+        Exercice=e[Math.floor(Math.random()*e.length)];
+        img.src='images/'+Exercice;
+        e.splice(e.indexOf(Exercice),1);
+        entrainement+='e'+'\t'+Exercice.substring(Exercice.search('/')+1,Exercice.length-4)+'\n'
+    }
+    else{
+        l.splice(l.indexOf(pre),1);
+        ch=l[Math.floor(Math.random()*l.length)];
+        c=eval(ch);
+        if (c.length==0){
+            eval('nouveau_'+ch+'()');
+            c=eval(ch);
+        };
+        console.log(c);
+        Exercice=c[Math.floor(Math.random()*c.length)];
+        img.src='images/'+Exercice;
+        c.splice(c.indexOf(Exercice),1);
+        pre=(ch);
+        l=['a','j','b_c'];
+        entrainement+=ch+'\t'+Exercice.substring(Exercice.search('/')+1,Exercice.length-4)+'\n'
+    };
+};
+
+function enregistrer(){
+    var today = new Date();
+    var a = document.createElement("a");
+    a.href = window.URL.createObjectURL(new Blob([entrainement], {type: "text/plain"}));
+    a.download = today.getDate()+"_"+('0000'+today.getMonth()+1).slice(-2)+"_"+today.getFullYear()+".txt";
+    a.click();
+}
