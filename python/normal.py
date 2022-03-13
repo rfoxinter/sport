993237b687f1ed186aa18400d01df858792fae73
rfoxinter <rfoxinter@gmail.com>
2022-03-13 12:08:13 +0100

    Update normal.py

--- python/normal.py
+++ python/normal.py
@@ -16,7 +16,7 @@ def nouveau_j():
     global j
     j=['jambes/'+i for i in listdir('images/jambes')]
 
-def nouveau():
+def nouveau(event=None):
     global root,a,a_tf,j,j_tf,b_c,b_c_tf,e,l,pre,Image,img,BoutonEx,n,entrainement
     n+=1
     if n<=4:
